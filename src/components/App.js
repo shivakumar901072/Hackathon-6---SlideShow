c451247cc5e46bf583808efca32faa930b649b94
shivakumar901072 <shivakumar901072@gmail.com>
2020-11-21 18:13:56 +0530

    slides

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React, {Component, useState} from "react";
+import slides from "../data"
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
+    <div>
+    <slides>
+    <h1>{slides.title}</h1>
+  <p>{slides.text}</p>
+  
+      <button data-testid="button-restart" onClick={}>Restart</button>
+      <button data-testid="button-prev" onClick={}>Next</button>
+        <button data-testid="button-next" onClick={}>prev</button>
+        </slides>
+      </div>
+      
   )
 }
 
