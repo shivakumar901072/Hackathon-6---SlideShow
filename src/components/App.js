e482ce2e5fdd82958ad0d15d0a819142416a3b5c
shivakumar901072 <shivakumar901072@gmail.com>
2020-11-21 19:14:52 +0530

    slides

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,12 @@ const App = () => {
     <div>
     <slides>
     <h1>{slides.title}</h1>
-  <p>{slides.text}</p>
-  
-      <button data-testid="button-restart" onClick={}>Restart</button>
-      <button data-testid="button-prev" onClick={}>Next</button>
-        <button data-testid="button-next" onClick={}>prev</button>
-        </slides>
-      </div>
-      
+    <p>{slides.text}</p>
+    <button data-testid="button-restart" onClick={slideshow()} disabled>Restart</button>
+    <button data-testid="button-next" onClick={slideshow()} disabled>Next</button>
+    <button data-testid="button-prev" onClick={slideshow()} disabled>Prev</button>
+    </slides>
+    </div>
   )
 }
 
