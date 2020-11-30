27abbdbfe49897632370ab214a612f2113e120a5
shivakumar901072 <shivakumar901072@gmail.com>
2020-11-30 23:23:10 +0530

    slideshow

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,64 @@
-import React, {Component, useState} from "react";
-import slides from "../data";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
+import Data from '../data.js';
+import Slides from './Slides.js';
 
-const App = () => {
+const App = (props) => {
+  const [title, setTitle] = useState(props.slides[0]["title"]);
+  const [text, setText] = useState(props.slides[0]["text"]);
+  const [count, setCount] = useState(0);
+  const [disableNext, setDesableNext] = useState(false);
+  const [disableprev, setDesableprev] = useState(true);
+  const [disableStart, setDesableStart]=useState(true);
+
+  const handleRestart = () => {
+    setTitle(props.slides[0]["title"]);
+    setText(props.slides[0]["text"]);
+    setDesableNext(false);
+    setDesableprev(true);
+    setDesableStart(true);
+  }
+  const handlePrev = () => {
+    
+    setDesableNext(false);
+    let slideIndx = count - 1;
+    if (slideIndx >= 0) {
+      setTitle(props.slides[slideIndx]["title"]);
+      setText(props.slides[slideIndx]["text"]);
+      setCount(slideIndx);
+    }
+    if(count===1){
+      setDesableprev(true);
+      setDesableStart(true);//update
+    }
+
+  }
+
+  const handleNext = () => {
+    setDesableprev(false);
+    setDesableStart(false);
+    let slideIndx = count + 1;
+    if (slideIndx < props.slides.length) {
+      setTitle(props.slides[slideIndx]["title"]);
+      setText(props.slides[slideIndx]["text"]);
+      setCount(slideIndx);
+    }
+    if (slideIndx === props.slides.length - 1) {
+      setDesableNext(true);
+    }
+
+  }
   return (
-    <div>
-    <slides>
-    <h1>{slides.title}</h1>
-    <p>{slides.text}</p>
-    <button data-testid="button-restart" onClick={slideshow()} disabled>Restart</button>
-    <button data-testid="button-next" onClick={slideshow()} disabled>Next</button>
-    <button data-testid="button-prev" onClick={slideshow()} disabled>Prev</button>
-    </slides>
-    </div>
+    <>
+      <Slides title={title} text={text}
+        disableNext={disableNext}
+        disablePrev={disableprev}
+        disableStart={disableStart}
+        handleRestart={handleRestart}
+        handlePrev={handlePrev}
+        handleNext={handleNext}
+      />
+    </>
   )
 }
 
